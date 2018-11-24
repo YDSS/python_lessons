f44a1b589638b51bc29b0d992d41175e4f7e3f01
arlenyang <arlenyang@tencent.com>
2018-11-24 17:01:20 +0800

    update

---
+++ cources/20181125/demo/catchBall/progress.py
@@ -0,0 +1,25 @@
+# encoding=utf-8
+
+# 多多接球游戏，面向过程思想实现
+
+def pickBall(master):
+    "捡球"
+    print "%s 捡起了一个球" % master
+
+def getAttention(master, dog):
+    "引起狗的注意，不然即使抛了球也可能不理你"
+    print "%s 引起了 %s 的注意" % (master, dog)
+
+def throwBall(master):
+    "抛球"
+    print "%s 抛出了一个球" % master
+
+def pickBallAndBack(dog):
+    "捡球"
+    print "%s 捡回了抛出去的球" % dog
+
+def startGame(master, dog):
+    master = "小魔女"
+    dog = "多多"
+
+    pickBall()
