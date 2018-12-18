1f81eff40178942e43e929c4d2b19ad7ee724fb6
arlenyang <arlenyang@tencent.com>
2018-12-18 19:30:00 +0800

    测试操作csv文件

--- practice/test.py
+++ practice/test.py
@@ -1,59 +1,34 @@
 # coding=utf-8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-class Person(object):
-    __id = '1234567' # 私有变量
-    ancestor = 'ape' # 静态变量
+import csv
+import time
 
-    def __init__(self, name, age, gender):
-        self.name = name # 成员变量
-        self.age = age
-        self.gender = gender
-    
-    def getId(self):
-        return self.__id[0:3] + '**' + self.__id[5:]
+def readFile():
+    with open('todo.csv') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        
+        todos = []
+        for row in reader:
+            todoDesc = ''
+            for i in range(len(row)):
+                val = row[i]
+                headerDesc = header[i]
+                todoDesc += (headerDesc + ": " + val + ", ")
 
-    # 静态方法
-    @staticmethod
-    def getClassName():
-        return "Person"
+            todos.append(todoDesc)
+            todoDesc = ""
 
-    # 成员方法
-    def talk(self, words):
-        print "%s said: %s" % (self.name, words)
+        print todos
 
-    # 私有方法
-    def __privateFunc(self):
-        print "test"
+# readFile()
 
-    # 使用私有方法 
-    def usePrivate(self):
-        self.__privateFunc()
+def writeRow():
+    with open('todo.csv', 'ab') as f:
+        writer = csv.writer(f)
+        d = time.time()
+        row = [1, 'lalala', 0, d, d]
 
-p = Person('pp', 23, 'male')
-print p.getId()
-# print Person.getClassName() # Person
-# p.usePrivate() # test
-# p.__privateFunc() # 报错
+        writer.writerow(row)
 
-
-# # print p.__wxNum
-# # print p.phoneNum
-# # Person.talk('1')
-# print Person.getClassName()
-
-# class Person(object):
-#     def __init__(self, name, age, gender):
-#         self.name = name
-#         self.age = age
-#         self.gender = gender
-#     def __del__(self):
-#         print "%s 被销毁了" % self.name 
-#     def talk(self, words):
-#         print "%s said: %s" % (self.name, words)
-
-# witch = Person('小魔女', 7, '不知道')
-# print witch.age
-# witch.talk('哦')
+writeRow()
